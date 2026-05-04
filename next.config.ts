d94f45a17453995c29f962752f1bc9a90948e90c
Ankita7107 <ankita.gholap@technfest.com>
2026-05-04 12:17:00 +0530

    changing ip

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins:['192.168.1.40']
+  allowedDevOrigins: ["192.168.1.39"],
 };
 
 export default nextConfig;
